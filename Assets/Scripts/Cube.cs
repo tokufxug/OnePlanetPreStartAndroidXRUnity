c8e659eb553fe359674af452e23f71f34cfc4459
tokufxug <tokufxug@gmail.com>
2024-12-15 21:59:28 +0900

    commit

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Android XR Unity Start!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Android XR Unity Update!");
+    }
+}
